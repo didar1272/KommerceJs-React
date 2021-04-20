569c3942b1172e141fa6d804450bad1ad6723871
Md. Didarul Islam <reurnzero101@gmail.com>
2021-04-20 17:56:32 +0600

    Cart Buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,14 +5,16 @@ import CartItem from './CartItem/CartItem'
 
 import useStyles from './styles'
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
 
     const classes = useStyles();
 
     const EmptyCart = () => (
+        <>
         <Typography variant='subtitle1'>You have no items in shopping cart, 
             <Link to='/' className={classes.link}>start assing some</Link>!!
         </Typography>
+        </>
     );
 
     const FilledCart = () => (
@@ -20,7 +22,8 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty}
+                        onRemoveFromCart={handleRemoveFromCart} />
                     </Grid>
                 ))}
             </Grid>
@@ -29,18 +32,20 @@ const Cart = ({ cart }) => {
                         Subtotal: {cart.subtotal.formatted_with_symbol}
                     </Typography>
                     <div>
-                        <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
+                        <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary' onClick={handleEmptyCart}>Empty Cart</Button>
                         <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Check Out</Button>
                     </div>
             </div>
         </>
     );
 
+    if(!cart.line_items) return 'Loading';
+
     return (
         <Container>
             <div className={classes.toolbar}/>
             <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
-            { !cart.line_items ? <EmptyCart /> : <FilledCart /> }
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
         </Container>
     )
 }
